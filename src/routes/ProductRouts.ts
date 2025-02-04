1c832467b8c3b463bdb0f8894f33ba413d6d5180
vedivero <vedivero@github.com>
2025-02-04 20:11:04 +0900

    feat/product - add AuthMiddleware.ts - 상품 등록 시, 토큰 검증 로직 추가

--- src/routes/ProductRouts.ts
+++ src/routes/ProductRouts.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getProducts, registProducts } from '../controllers/ProductController';
+import { verifyToken } from '../middleware/AuthMiddleware';
 
 const router = express.Router();
 
 router.get('/products', getProducts);
-router.post('/regist', registProducts);
+router.post('/regist', verifyToken, registProducts);
 
 export default router;
