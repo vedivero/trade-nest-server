868b876c0b17ada67882fa8496b37a688432d67b
vedivero <vedivero@github.com>
2025-02-06 21:20:12 +0900

    feat/login - 회원가입(이메일 인증) 및 로그인 기능 구현

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
-import { generateTokens, handleSocialLogin } from '../services/AuthService';
+import {
+   generateTokens,
+   handleEmailLogin,
+   handleEmailSignUp,
+   handleSocialLogin,
+   verifyEmailToken,
+} from '../services/AuthService';
 import httpStatus from 'http-status';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -34,5 +40,50 @@ export const socialLogin = async (req: Request, res: Response): Promise<void> =>
    } catch (error) {
       console.error('❌ 요청 처리 중 오류 발생:', error);
       res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: error || '서버 오류' });
+   }
+};
+
+export const emailSignUp = async (req: Request, res: Response): Promise<void> => {
+   try {
+      const { email, password, nickname } = req.body;
+
+      if (!email || !password || !nickname) {
+         res.status(httpStatus.BAD_REQUEST).json({ message: '필수 정보를 입력해 주세요.' });
+      }
+
+      await handleEmailSignUp({ email, password, nickname });
+      res.status(httpStatus.OK).json({ message: '해당 이메일 주소로 인증 메일을 전송하였습니다.' });
+   } catch (error) {
+      console.error('❌ 이메일 회원가입 중 오류 발생:', error);
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+         message: '회원가입 처리 중 오류가 발생했습니다.',
+         error,
+      });
+   }
+};
+
+export const verifyEmail = async (req: Request, res: Response): Promise<void> => {
+   try {
+      const { token } = req.query;
+      const isVerified = await verifyEmailToken(token as string);
+
+      if (!isVerified) {
+         res.status(httpStatus.BAD_REQUEST).json({ message: '유효하지 않은 토큰입니다.' });
+      }
+      res.status(httpStatus.OK).json({ message: '이메일이 인증되었습니다.', status: 'success' });
+   } catch (error) {
+      console.error('❌ 이메일 인증 중 오류 발생:', error);
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: '이메일 인증 실패', error });
+   }
+};
+
+export const emailLogin = async (req: Request, res: Response): Promise<void> => {
+   try {
+      const { email, password } = req.body;
+      const tokens = await handleEmailLogin({ email, password });
+      res.status(httpStatus.OK).json(tokens);
+   } catch (error) {
+      console.error('❌ 로그인 중 오류 발생:', error);
+      res.status(httpStatus.UNAUTHORIZED).json({ message: error });
    }
 };
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,16 @@
 import { IUser } from '../Model/User';
-import { createUser, findUserBySocialId } from '../repositories/UserRepository';
+import {
+   createUser,
+   findUserByEmail,
+   findUserBySocialId,
+   updateUserVerification,
+} from '../repositories/UserRepository';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+
 dotenv.config();
 
 interface SocialLoginData {
@@ -48,4 +57,66 @@ export const generateTokens = (user: IUser) => {
    const refreshToken = jwt.sign(payload, refreshSecret, { expiresIn: '7d' });
 
    return { accessToken, refreshToken };
+};
+
+const transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+   },
+});
+
+export const handleEmailSignUp = async ({
+   email,
+   password,
+   nickname,
+}: {
+   email: string;
+   password: string;
+   nickname: string;
+}) => {
+   if (await findUserByEmail(email)) {
+      throw new Error('이미 가입된 이메일입니다.');
+   }
+
+   const hashedPassword = await bcrypt.hash(password, 10);
+   const emailToken = crypto.randomBytes(32).toString('hex');
+
+   await createUser({
+      user_id: `user_${Date.now()}`,
+      email,
+      password: hashedPassword,
+      nickname,
+      social_provider: 'self',
+      social_id: email,
+      verified: false,
+      emailToken,
+   });
+
+   const verifyLink = `${process.env.FRONTEND_URL}/verify-email?token=${emailToken}`;
+
+   await transporter.sendMail({
+      from: process.env.EMAIL_ADDRESS,
+      to: email,
+      subject: '이메일 인증 요청',
+      html: `<p>이메일 인증을 위해 아래 링크를 클릭해주세요:</p>
+      <a href="${verifyLink}">${verifyLink}</a>`,
+   });
+};
+
+export const verifyEmailToken = async (token: string): Promise<boolean> => {
+   return await updateUserVerification(token);
+};
+
+export const handleEmailLogin = async ({ email, password }: { email: string; password: string }) => {
+   const user = await findUserByEmail(email);
+
+   if (!user) throw new Error('존재하지 않는 사용자입니다.');
+   if (!user.verified) throw Error('이메일 인증이 필요합니다.');
+
+   const isValidPassword = await bcrypt.compare(password, user.password);
+   if (!isValidPassword) throw new Error('비밀번호가 일치하지 않습니다');
+
+   return generateTokens(user);
 };
